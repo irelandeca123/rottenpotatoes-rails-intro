919b77d0c3be8104e288ed90413b57142c5a3de7
Stoyan Rizov <stoyanrizov6@gmail.com>
2019-02-25 10:26:27 +0000

    Added comments

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+    #Defining as class method(Self Ratings), Using distinct method taking each rating , pushing each element to array. 
     def self.ratings
         Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
